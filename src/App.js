e9ae0fc396739a47dd9d542c74e196dcb32a5a21
greymanking <greyman.king@gmail.com>
2018-11-29 21:32:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+var records = [
+  { ID:1, word: "build", meaning: "建造" },
+  { ID:2, word: "destroy", meaning: "破坏" },
+]
+
+function Page(props) {
+  return (
+    <div>
+    <h3>{props.word}</h3>
+    <div>{props.meaning}</div>
+    </div>
+  );
+}
+
+function Pages() {
+  return records.map((r)=>{
+    return <Page key={r.ID} word={r.word} meaning={r.meaning} />
+  });
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Pages />
       </div>
     );
   }
 }
+
 
 export default App;
