70fe096ab3645b78bdb0179b90a5d901953041e4
greymanking <greyman.king@gmail.com>
2019-01-23 22:05:00 +0800

    tidy up ending stage

--- src/component/ending.js
+++ src/component/ending.js
@@ -31,28 +31,31 @@ class Ending extends Component {
     return (
       <div className='content bgpeace'>
         <div className='min_page'>
-          {us === ULSTATUS.going && <FontAwesomeIcon icon='spinner' className='load_ani' />}
-          {us === ULSTATUS.fail && <FontAwesomeIcon icon='exclamation-triangle' className='colorred' />}
+          {us === ULSTATUS.going && <FontAwesomeIcon icon='spinner' className='load_ani fontextralarge' />}
+          {us === ULSTATUS.fail && <FontAwesomeIcon icon='exclamation-triangle' className='colorred fontextralarge' />}
           {us === ULSTATUS.done && <div>
-            <h3>
-              你已完成本轮练习！<br />
-              <br />
+            <h3>你已完成本轮练习！</h3>
+            <table><tbody>
               {pl !== 0 && (
-                <React.Fragment>
-                  <FontAwesomeIcon icon='puzzle-piece' />
-                  {pg}/{pl} 胜率{(pg / pl * 100).toFixed(0)}%
-                </React.Fragment>
+                <tr>
+                  <td><FontAwesomeIcon icon='puzzle-piece' /></td>
+                  <td>{pg}/{pl}</td><td>胜率{(pg / pl * 100).toFixed(0)}%</td>
+                </tr>
               )}
-              <br />
-              <FontAwesomeIcon icon='keyboard' />
-              {dg}/{dl} 胜率{(dg / dl * 100).toFixed(0)}%
-              <br />
-              <FontAwesomeIcon icon='yen-sign' />
-              <br />
-              <FontAwesomeIcon icon='gem' />
-              <br />
-              目前级别：
-            </h3>
+              <tr>
+                <td><FontAwesomeIcon icon='keyboard' /></td>
+                <td>{dg}/{dl}</td><td>胜率{(dg / dl * 100).toFixed(0)}%</td>
+              </tr>
+              <tr>
+                <td><FontAwesomeIcon icon='yen-sign' /></td>
+              </tr>
+              <tr>
+                <td><FontAwesomeIcon icon='gem' /></td>
+              </tr>
+              <tr>
+                <td></td><td>目前级别</td>
+              </tr>
+            </tbody></table>
             <button className='primary' onClick={this.props.nextrun}>再来一轮吧</button>
             <button className='primary marginleft' onClick={this.props.doFallible}>复习易错题</button>
           </div>
